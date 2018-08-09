89b5334be588a3bfc562727b3065c3e3b9006213
Douglas Kogut <dkogut@mit.edu>
2018-08-09 10:44:49 -0700

    conn reset by peer when trying to do concurrent load tests

--- examples/initiator/src/main/java/com/paritytrading/philadelphia/initiator/TestInitiator.java
+++ examples/initiator/src/main/java/com/paritytrading/philadelphia/initiator/TestInitiator.java
@@ -23,7 +23,7 @@ class TestInitiator {
             int    port   = Integer.parseInt(args[1]);
             int    orders = Integer.parseInt(args[2]);
 
-            main(new InetSocketAddress(host, port), orders);
+            main(new InetSocketAddress(host, port), orders, host, port);
         } catch (IllegalArgumentException e) {
             usage(USAGE);
         } catch (IOException e) {
@@ -31,61 +31,156 @@ class TestInitiator {
         }
     }
 
-    private static void main(InetSocketAddress address, int orders) throws IOException {
-        Initiator initiator = Initiator.open(address);
-
-        long nextClOrdId = 1;
-
-        FIXMessage message = initiator.getTransport().create();
-
-        initiator.getTransport().prepare(message, OrderSingle);
-
-        FIXValue clOrdId = message.addField(ClOrdID);
-
-        message.addField(HandlInst).setChar(HandlInstValues.AutomatedExecutionNoIntervention);
-        message.addField(Symbol).setString("FOO");
-        message.addField(Side).setChar(SideValues.Buy);
-
-        FIXValue transactTime = message.addField(TransactTime);
-        transactTime.setString(initiator.getTransport().getCurrentTimestamp());
-
-        message.addField(OrderQty).setFloat(100.00, 2);
-        message.addField(OrdType).setChar(OrdTypeValues.Limit);
-        message.addField(Price).setFloat(25.50, 2);
-
+    private static void main(InetSocketAddress address, int orders, String host, int port) throws IOException {
         System.out.println("Warming up...");
 
-        for (int i = 0; i < orders; i++) {
-            clOrdId.setInt(nextClOrdId++);
+        Thread t[] = new Thread[36];
+        Initiator inits[] = new Initiator[37];
+        Thread rem = new Thread();
+        for (int j = 0; j <= 36; j++) {
+          InetSocketAddress addr = new InetSocketAddress(host, port);
+          inits[j] = Initiator.open(addr);
+        }
+        for (int j = 0; j <= 36; j++) {
+          long nextClOrdId = 1+j*(orders/36);
 
-            initiator.send(message);
+          FIXMessage message = inits[j].getTransport().create();
 
-            initiator.receive();
+          inits[j].getTransport().prepare(message, OrderSingle);
+
+          FIXValue clOrdId = message.addField(ClOrdID);
+
+          message.addField(HandlInst).setChar(HandlInstValues.AutomatedExecutionNoIntervention);
+          message.addField(Symbol).setString("FOO");
+          message.addField(Side).setChar(SideValues.Buy);
+
+          FIXValue transactTime = message.addField(TransactTime);
+          transactTime.setString(inits[j].getTransport().getCurrentTimestamp());
+
+          message.addField(OrderQty).setFloat(100.00, 2);
+          message.addField(OrdType).setChar(OrdTypeValues.Limit);
+          message.addField(Price).setFloat(25.50, 2);
+          if (j < 36) {
+            t[j] = new ParallelRequest(orders/36, inits[j], message);
+          } else {
+            if (orders%36 == 0) {
+              rem = new ParallelRequest(orders%36, inits[j], message);
+            }
+          }
+        }
+        for (int j = 0; j < 36; j++) {
+          t[j].start();
+        }
+        if (orders%36 == 0) {
+          rem.start();
+        }
+        for (int i = 0; i < 36; i++) {
+          try {
+            t[i].join();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+        try {
+          rem.join();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
         }
 
-        initiator.getHistogram().reset();
+        for (int j = 0; j <= 36; j++) {
+          inits[j].getHistogram().reset();
+        }
 
         System.out.println("Benchmarking...");
 
-        for (int i = 0; i < orders; i++) {
-            clOrdId.setInt(nextClOrdId++);
+        long start = System.nanoTime();
+        for (int j = 0; j <= 36; j++) {
+          long nextClOrdId = 1+j*(orders/36);
 
-            initiator.send(message);
+          FIXMessage message = inits[j].getTransport().create();
 
-            initiator.receive();
+          inits[j].getTransport().prepare(message, OrderSingle);
+
+          FIXValue clOrdId = message.addField(ClOrdID);
+
+          message.addField(HandlInst).setChar(HandlInstValues.AutomatedExecutionNoIntervention);
+          message.addField(Symbol).setString("FOO");
+          message.addField(Side).setChar(SideValues.Buy);
+
+          FIXValue transactTime = message.addField(TransactTime);
+          transactTime.setString(inits[j].getTransport().getCurrentTimestamp());
+
+          message.addField(OrderQty).setFloat(100.00, 2);
+          message.addField(OrdType).setChar(OrdTypeValues.Limit);
+          message.addField(Price).setFloat(25.50, 2);
+          if (j < 36) {
+            t[j] = new ParallelRequest(orders/36, inits[j], message);
+          } else {
+            if (orders%36 == 0) {
+              rem = new ParallelRequest(orders%36, inits[j], message);
+            }
+          }
         }
+        for (int j = 0; j < 36; j++) {
+          t[j].start();
+        }
+        if (orders%36 == 0) {
+          rem.start();
+        }
+        for (int i = 0; i < 36; i++) {
+          try {
+            t[i].join();
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+        }
+        try {
+          rem.join();
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+        long end = System.nanoTime();
 
-        initiator.getTransport().close();
+        for (int j = 0; j <= 36; j++) {
+          inits[j].getTransport().close();
+        }
 
         System.out.printf("Results (n = %d)\n", orders);
         System.out.printf("\n");
-        System.out.printf("   50.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 50.00) / 1000.0);
-        System.out.printf("   90.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 90.00) / 1000.0);
-        System.out.printf("   99.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 99.00) / 1000.0);
-        System.out.printf("   99.90%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 99.90) / 1000.0);
-        System.out.printf("   99.99%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 99.99) / 1000.0);
-        System.out.printf("  100.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile(100.00) / 1000.0);
+        // System.out.printf("   50.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 50.00) / 1000.0);
+        // System.out.printf("   90.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 90.00) / 1000.0);
+        // System.out.printf("   95.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 95.00) / 1000.0);
+        // System.out.printf("   99.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 99.00) / 1000.0);
+        // System.out.printf("   99.90%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 99.90) / 1000.0);
+        // System.out.printf("   99.99%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile( 99.99) / 1000.0);
+        // System.out.printf("  100.00%%: %10.2f µs\n", initiator.getHistogram().getValueAtPercentile(100.00) / 1000.0);
+        System.out.printf("  Avg. t/p: %10.2f rps\n", orders / ((end - start) / 1000000000.0));
         System.out.printf("\n");
     }
 
+}
+
+class ParallelRequest extends Thread {
+    private int num;
+    private Initiator in;
+    private FIXMessage mes;
+    public ParallelRequest(int numOrders, Initiator i, FIXMessage m) {
+      num = numOrders;
+      in = i;
+      mes = m;
+    }
+
+    @Override
+    public void run() {
+      try {
+        for (int i = 0; i < num; i++) {
+            in.send(mes);
+
+            in.receive();
+            System.out.println("hi");
+        }
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+    }
 }
